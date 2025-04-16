33ffdd537f5c57f399cd064f31a58a78169c3f76
lujain <loginalkholy@gmail.com>
2025-04-16 14:20:05 +0200

    day 3

---
+++ day3.dart
@@ -0,0 +1,44 @@
+// task (1)
+
+// class Car {
+//   String brand;
+//   String model;
+//   int year;
+//   Car(this.brand, this.model, this.year);
+//   void displayInfo() {
+//     print('Brand: $brand, Model: $model, Year: $year');
+//   }
+// }
+// void main() {
+//   var car1 = Car('Tesla', 'Model S', 2022);
+//   car1.displayInfo(); 
+// }
+
+//------------------------------------------------------------
+
+// task (2)
+// class Animal {
+//   void makeSound() {   
+//   }
+// }
+// class Dog extends Animal {
+  
+//   void makeSound() {
+//     print('Dog barks');
+//   }
+// }
+// class Cat extends Animal {
+//   void makeSound() {
+//     print('Cat meows');
+//   }
+// }
+// void main() {
+//   var dog = Dog();
+//   var cat = Cat();
+//   dog.makeSound(); 
+//   cat.makeSound(); 
+// }
+
+
+// ------------------------------------------------------------
+// task (3)
